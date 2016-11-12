fccff2811a82cc8946488b222397955193727819
Kenta Sato <bicycle1885@gmail.com>
2016-11-12 17:00:43 +0900

    add an example of XMLReader

---
+++ example/graphml.jl
@@ -0,0 +1,38 @@
+using LightGraphs
+using EzXML
+
+function load_graphml(filename)
+    open(XMLReader, filename) do reader
+        serial = 0
+        nodes = Dict{String,Int}()
+        edges = Pair{Int,Int}[]
+
+        # scan nodes
+        for typ in reader
+            if typ == EzXML.XML_READER_TYPE_ELEMENT
+                elname = name(reader)
+                if elname == "node"
+                    serial += 1
+                    nodes[reader["id"]] = serial
+                elseif elname == "edge"
+                    source_id = nodes[reader["source"]]
+                    target_id = nodes[reader["target"]]
+                    push!(edges, source_id => target_id)
+                end
+            end
+        end
+
+        # build an undirected graph
+        graph = Graph(serial)
+        for edge in edges
+            add_edge!(graph, edge)
+        end
+
+        # return the graph and the ID-to-name mapping
+        return graph, Dict(id => name for (name, id) in nodes)
+    end
+end
+
+graph, idmap = load_graphml(ARGS[1])
+@show graph
+@show idmap
