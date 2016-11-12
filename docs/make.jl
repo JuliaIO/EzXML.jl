4eb7fe2119f05c1596bdcd0b9db15a5b6deb81dc
Kenta Sato <bicycle1885@gmail.com>
2016-11-12 19:36:43 +0900

    add Documenter deployment files

--- docs/make.jl
+++ docs/make.jl
@@ -5,3 +5,7 @@ makedocs(
     format=:html,
     sitename="EzXML.jl",
     pages=["manual.md", "references.md", "devnotes.md"])
+
+deploydocs(
+    repo="github.com/bicycle1885/EzXML.jl.git",
+    julia="0.5")
