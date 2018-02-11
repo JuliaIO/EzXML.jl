fb4e964f5e5b06fa8c1bf6ee874ac68da2f785ff
autodocs <autodocs>
2018-02-11 07:33:05 +0000

    build based on cc828de

--- versions.js
+++ versions.js
@@ -1,10 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.6",
   "release-0.5",
   "release-0.4",
   "release-0.3",
   "release-0.2",
+  "v0.6.0",
   "v0.5.6",
   "v0.5.5",
   "v0.5.4",
